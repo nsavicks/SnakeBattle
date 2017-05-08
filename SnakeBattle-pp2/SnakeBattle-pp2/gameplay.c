c9b15cd6ea39551e0167f8b56ad1d98bbb4b096c
nsavicks <nebojsasavic6@gmail.com>
2017-05-09 00:11:36 +0200

    Igra se zavrsava kad sve umru i ispravka da ne moze promeni smer u suprotnom pravcu

--- SnakeBattle-pp2/SnakeBattle-pp2/gameplay.c
+++ SnakeBattle-pp2/SnakeBattle-pp2/gameplay.c
@@ -27,8 +27,9 @@ zmija nextMove(zmija z,int mapa[][100],int n) {
 		else {
 			mapa[z.glava.i - 1][z.glava.j] = z.redni;
 			z.duzina++;
-			z.telo[z.duzina - 1] = z.glava;
+			
 			z.glava.i--;
+			z.telo[z.duzina - 1] = z.glava;
 		}
 		break;
 	case DESNO:
@@ -38,8 +39,9 @@ zmija nextMove(zmija z,int mapa[][100],int n) {
 		else {
 			mapa[z.glava.i][z.glava.j+1] = z.redni;
 			z.duzina++;
-			z.telo[z.duzina - 1] = z.glava;
+			
 			z.glava.j++;
+			z.telo[z.duzina - 1] = z.glava;
 		}
 		break;
 	case DOLE:
@@ -49,8 +51,9 @@ zmija nextMove(zmija z,int mapa[][100],int n) {
 		else {
 			mapa[z.glava.i + 1][z.glava.j] = z.redni;
 			z.duzina++;
-			z.telo[z.duzina - 1] = z.glava;
+			
 			z.glava.i++;
+			z.telo[z.duzina - 1] = z.glava;
 		}
 		break;
 	case LEVO:
@@ -60,8 +63,8 @@ zmija nextMove(zmija z,int mapa[][100],int n) {
 		else {
 			mapa[z.glava.i][z.glava.j - 1] = z.redni;
 			z.duzina++;
-			z.telo[z.duzina - 1] = z.glava;
 			z.glava.j--;
+			z.telo[z.duzina - 1] = z.glava;
 		}
 		break;
 
@@ -84,8 +87,9 @@ void play(zmija zm1, zmija zm2, zmija zm3, zmija zm4,int mapa[][100],int n,int b
 	
 	int zivih = zm1.ziva + zm2.ziva + zm3.ziva + zm4.ziva;
 	SDL_Event e;
-
+	
 		while (zivih>0) {
+			ispis(window, renderer, zm1, zm2, zm3, zm4, mapa, n);
 			SDL_Delay(brzina);
 			printf("%d\n", zm1.smer);
 			while (SDL_PollEvent(&e))
@@ -93,27 +97,35 @@ void play(zmija zm1, zmija zm2, zmija zm3, zmija zm4,int mapa[][100],int n,int b
 				case SDL_KEYDOWN:
 					switch (e.key.keysym.sym) {
 					case SDLK_UP:
+						if (zm1.smer!=DOLE)
 						zm1.smer = GORE;
 						break;
 					case SDLK_DOWN:
+						if (zm1.smer!=GORE)
 						zm1.smer = DOLE;
 						break;
 					case SDLK_LEFT:
+						if (zm1.smer!=DESNO)
 						zm1.smer = LEVO;
 						break;
 					case SDLK_RIGHT:
+						if (zm1.smer!=LEVO)
 						zm1.smer = DESNO;
 						break;
 					case SDLK_w:
+						if (zm2.smer!=DOLE)
 						zm2.smer = GORE;
 						break;
 					case SDLK_s:
+						if (zm2.smer!=GORE)
 						zm2.smer = DOLE;
 						break;
 					case SDLK_a:
+						if (zm2.smer!=DESNO)
 						zm2.smer = LEVO;
 						break;
 					case SDLK_d:
+						if (zm2.smer!=LEVO)
 						zm2.smer = DESNO;
 						break;
 					default:
@@ -127,8 +139,8 @@ void play(zmija zm1, zmija zm2, zmija zm3, zmija zm4,int mapa[][100],int n,int b
 			if (zm2.ziva) zm2 = nextMove(zm2, mapa, n);
 			if (zm3.ziva) zm3 = nextMove(zm3, mapa, n);
 			if (zm4.ziva) zm4 = nextMove(zm4, mapa, n);
+			zivih = zm1.ziva + zm2.ziva + zm3.ziva + zm4.ziva;
 			
-			ispis(window, renderer, zm1, zm2, zm3, zm4, mapa, n);
 		}
 
 }
--- SnakeBattle-pp2/SnakeBattle-pp2/main.c
+++ SnakeBattle-pp2/SnakeBattle-pp2/main.c
@@ -10,7 +10,7 @@ int main() {
 	SDL_Renderer *rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 	zmija zm1,zm2,zm3,zm4;
-	zm2.ziva = zm3.ziva = zm4.ziva = 0;
+	zm3.ziva = zm4.ziva = 0;
 	VEL = 40;
 	int n = 20;
 	int mapa[100][100] = { 0 };
@@ -21,6 +21,14 @@ int main() {
 	zm1.telo[0] = zm1.glava;
 	zm1.ziva = 1;
 	mapa[5][5] = 1;
+
+	zm2.duzina = 1;
+	zm2.glava.i = zm2.glava.j = zm2.rep.i = zm2.rep.j = 10;
+	zm2.redni = 2;
+	zm2.smer = LEVO;
+	zm2.telo[0] = zm2.glava;
+	zm2.ziva = 1;
+	mapa[10][10] = 2;
 	int brzina = 1000;
 
 	play(zm1, zm2, zm3, zm4, mapa, n, brzina, window, rend);
