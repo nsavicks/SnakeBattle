74c960c2e065a0dc560f100e4b46b8e5006d17e5
nsavicks <nebojsasavic6@gmail.com>
2017-05-08 20:32:36 +0200

    kill i nextMove

--- SnakeBattle-pp2/SnakeBattle-pp2/gameplay.c
+++ SnakeBattle-pp2/SnakeBattle-pp2/gameplay.c
@@ -7,23 +7,63 @@ struct ko {
 struct zm {
 	koord rep, glava, telo[100];
 	int duzina;
-	short smer, mrtva, igrac, tezina;
+	short smer, mrtva, igrac, tezina,redni;
 };
 
 
-zmija nextMove(zmija z, int komanda,int mapa[][100]) {
+zmija kill(zmija z, int mapa[][100]) {
+
+	int i;
+	for (i = 0; i < z.duzina; i++) {
+		mapa[z.telo[i].i][z.telo[i].j] = 0;
+	}
+	z.glava.i = z.glava.j = z.rep.i = z.rep.j = -1;
+	z.mrtva = 1;
+	return z;
+}
+
+zmija nextMove(zmija z, int komanda,int mapa[][100],int n) {
 
 	switch (komanda) {
 	case GORE: 
-		if (mapa[z.glava.i - 1][z.glava.j]) { printf("NE"); }
+		if ((z.glava.i == 0) || (mapa[z.glava.i - 1][z.glava.j])) {
+			z = kill(z,mapa);
+		}
+		else {
+			mapa[z.glava.i - 1][z.glava.j] = z.redni;
+			z.glava.i--;
+		}
 		break;
 	case DESNO:
+		if ((z.glava.j == n-1) || (mapa[z.glava.i][z.glava.j+1])) {
+			z = kill(z, mapa);
+		}
+		else {
+			mapa[z.glava.i][z.glava.j+1] = z.redni;
+			z.glava.j++;
+		}
 		break;
 	case DOLE:
+		if ((z.glava.i == n-1) || (mapa[z.glava.i + 1][z.glava.j])) {
+			z = kill(z, mapa);
+		}
+		else {
+			mapa[z.glava.i + 1][z.glava.j] = z.redni;
+			z.glava.i++;
+		}
 		break;
 	case LEVO:
+		if ((z.glava.j == 0) || (mapa[z.glava.i][z.glava.j - 1])) {
+			z = kill(z, mapa);
+		}
+		else {
+			mapa[z.glava.i][z.glava.j - 1] = z.redni;
+			z.glava.j--;
+		}
 		break;
-	
+
+	default: break;
 	}
 
+	return z;
 }
