cd83e5a1f3899d9255d7cd697521587adc9a56f6
nsavicks <nebojsasavic6@gmail.com>
2017-05-08 19:15:45 +0200

    gameplay.h i c

---
+++ SnakeBattle-pp2/SnakeBattle-pp2/gameplay.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef gameplay
+#define gameplay
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "grafika.h"
+
+
+
+
+
+#endif 
--- SnakeBattle-pp2/SnakeBattle-pp2/main.c
+++ SnakeBattle-pp2/SnakeBattle-pp2/main.c
@@ -1,10 +1,6 @@
 #include <SDL.h>
 
 int main(int **argc, char* args[]) {
-	while (1) {
-
-		printf("SADASDA");
-	}
-
+	
 	return 0;
 }
